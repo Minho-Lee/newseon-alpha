2bbee11f47a46a702a9bd16acc42a606a8926cd2
bdcorps <bdcorps@gmail.com>
2018-07-28 21:33:28 -0400

    update minor button hover color; update google callback url

--- auth/google.js
+++ auth/google.js
@@ -3,16 +3,16 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../models/user_model');
 
 passport.use(new GoogleStrategy({
-    clientID: "668895022164-tcfccvvs5jqrj21jqqqnv46ici1g5env.apps.googleusercontent.com",
-    clientSecret: "WoXTSjq1bP6lWkBoNMrPBvtj",
-    callbackURL: "http://localhost:6008/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-console.log(profile);
+  clientID: "668895022164-tcfccvvs5jqrj21jqqqnv46ici1g5env.apps.googleusercontent.com",
+  clientSecret: "WoXTSjq1bP6lWkBoNMrPBvtj",
+  callbackURL: "https://newseon.io/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    console.log(profile);
 
-       User.findOrCreate({ userid: profile.id }, { name: profile.displayName,userid: profile.id, email:profile.emails[0].value, profile_url: profile.photos[0].value }, function (err, user) {
-         return done(err, user);
-       });
+    User.findOrCreate({ userid: profile.id }, { name: profile.displayName, userid: profile.id, email: profile.emails[0].value, profile_url: profile.photos[0].value }, function (err, user) {
+      return done(err, user);
+    });
   }
 ));
 
